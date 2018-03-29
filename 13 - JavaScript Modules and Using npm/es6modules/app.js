f6e42d2f18101739d58789ce3a9b5f9b76d5f3f2
Andy D. Nguyen <andy.d.nguyen9@gmail.com>
2018-03-28 22:18:09 -0500

    finish more es6 module practice

--- 13 - JavaScript Modules and Using npm/es6modules/app.js
+++ 13 - JavaScript Modules and Using npm/es6modules/app.js
@@ -2,6 +2,7 @@ import { uniq } from 'lodash';
 import insane from 'insane';
 import jsonp from 'jsonp';
 import { fakeApiKey, url, sayHi, old, dog } from './src/config';
+import User, { createURL, gravatar } from './src/user';
 
 console.log(fakeApiKey);
 console.log(url);
@@ -11,3 +12,10 @@ console.log(old, dog);
 const ages = [1, 1, 4, 52, 12, 4];
 
 console.log(uniq(ages));
+
+const wes = new User('Wes Bos', 'wesbos@gmail.com', 'wesbos.com');
+const profile = createURL(wes.name);
+const image = gravatar(wes.email);
+console.log(wes);
+console.log(profile);
+console.log(image);
