9a459ea347ff10ec8816e70a724a7dbaa139f51f
Andy D. Nguyen <andy.d.nguyen9@gmail.com>
2018-03-28 22:05:12 -0500

    finish creating your own modules

--- 13 - JavaScript Modules and Using npm/es6modules/app.js
+++ 13 - JavaScript Modules and Using npm/es6modules/app.js
@@ -1,6 +1,12 @@
 import { uniq } from 'lodash';
 import insane from 'insane';
 import jsonp from 'jsonp';
+import { fakeApiKey, url, sayHi, old, dog } from './src/config';
+
+console.log(fakeApiKey);
+console.log(url);
+sayHi('Andy');
+console.log(old, dog);
 
 const ages = [1, 1, 4, 52, 12, 4];
 
